5a89541ca7b373d52f999ccd7ce3f5d08d609425
NobodySpecial <123022316+NS57@users.noreply.github.com>
2024-06-04 14:12:03 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@ import data from './posts.json' assert { type: 'json' };
 
 let strData = data.toString();
 const para = document.createElement("p");
-const node = document.createTextNode(data);
+const node = document.createTextNode(strData);
 para.appendChild(node);
 
 const container = document.getElementById('container');
