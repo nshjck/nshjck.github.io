791f52988753d8e698f3859cb4c4f48ae7c0d990
NS <jackhindson@gmail.com>
2024-09-08 17:27:46 +0100

    more stuff idk

--- scripts.js
+++ scripts.js
@@ -4,4 +4,19 @@ el.onclick = myFunction;
 function myFunction() {
     var element = document.getElementById('main');
     element.classList.toggle('new-theme');
+    cursor()
+}
+
+//thanks bee for the cursor :3
+function cursor() {
+    window.addEventListener("load", (event) => {
+      new cursoreffects.fairyDustCursor({
+      colors: ["#D60270", "#9B4F96", "#0038A8"],
+    });
+    new cursoreffects.rainbowCursor({
+      colors: ["#D60270", "#9B4F96", "#0038A8"],
+      length: "35",
+      size: "5  ",
+    })
+    });
 }
