af37e42338da4c545941b82ffd6be93f33274c3f
NS <jackhindson@gmail.com>
2024-09-08 15:48:42 +0100

    kewl new stuff

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var el = document.getElementById(':3');
+el.onclick = myFunction;
+
+function myFunction() {
+    var element = document.getElementById('main');
+    element.classList.toggle('new-theme');
+}
